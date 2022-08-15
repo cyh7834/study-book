cad1cd7a31a7b93881fffda3cf2ee9f3563d40a2
Yoonho Choi <cyh7834@gmail.com>
2022-08-15 15:06:40 +0900

    1.1.1 싱글톤 패턴

---
+++ 면접을 위한 CS 전공지식 노트/1장 디자인 패턴/Section 1 디자인 패턴/1.1.1 싱글톤 패턴/Singleton.java
@@ -0,0 +1,9 @@
+public class Singleton {
+    private static class singleInstanceHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static synchronized Singleton getInstance() {
+        return singleInstanceHolder.INSTANCE;
+    }
+}
